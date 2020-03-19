11a9ebe7303131b737e13f1b3c2af4e4c47b659c
Michael Pedruski <mpedruski@gmail.com>
2020-03-19 11:24:21 -0400

    Generates network of character connections using token threshold and  mean cooccurences

--- general.py
+++ general.py
@@ -1,29 +1,50 @@
 import nltk as nltk
 import numpy as np
+import itertools
+
+def list_comparer(lists, threshold):
+    '''[[int]] -> arr
+    Accepts a list of lists of integers and identifies how many elements exist
+    within all pairs of lists that are within a threshold distance.
+    The number of elements within this threshold is returned in the form of a
+    numpy array.
+    '''
+    comparisons = np.zeros((len(lists),len(lists)))
+    for i in range(len(lists)):
+        list_a = lists[i]
+        for j in range(len(lists)):
+            list_b = lists[j]
+            if i!=j:
+                # print("i: {}, j:{}, val:{}".format(i,j,sum([a == b for (a, b) in itertools.product(list_a, list_b)])))
+                comparisons[i,j] = (sum([abs(a - b) < threshold for (a, b) in itertools.product(list_a, list_b)]))
+    return comparisons
+
+### Load file and pre-process text
 fh = open('./data/general.txt','r')
 raw = fh.read().lower()
-# raw = 'The brown cow jumped over the brown moon'
 tokens = nltk.word_tokenize(raw)
-
-### Stopwords on hand, but is their removal needed?
-# final_stops = stopwords.words('french')
-
 text = nltk.Text(tokens)
 
-characters = ['général','dabrovine','papofski','jackson','dérigny','natasha']
+### Include a list of characters whose relationships should be analyzed
+### and determine where in the text they occur
+names = ['général','dabrovine','papofski','dérigny','natasha',
+    'jacques', 'paul', 'romane', 'pajarski','jackson']
 locations = []
-
-for i in characters:
+for i in names:
     locations.append([j for j, item in enumerate(text) if item == i])
-print('Length of character locations vector: {}'.format(len(locations)))
 
-count = [0,0,0,0,0]
-for i in range(1,len(characters)):
-    print(i)
-    character_0 = locations[0]
-    character_1 = locations[i]
-    for j in character_0:
-        for k in character_1:
-            if abs(j-k) < 20:
-                count[i-1] += 1
-print(count)
+### Combine jackson, pajarski, and romane as synonyms
+jackson = locations[-3]+locations[-2]+locations[-1]
+locations = locations[:-3]
+locations.append(jackson)
+### How close in the text should character tokens have to be to be counted?
+threshold = 20
+
+### Count cooccurrences and then determine which of these are important links
+a = list_comparer(locations, threshold)
+print('Matrix of cooccurrences:\n')
+print(a)
+
+### Define links to be where cooccurrences are greater than average cooccurrences
+print('Connection matrix defined by mean cooccurrences:\n')
+print(np.greater(a,np.mean(a)))
